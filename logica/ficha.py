61b178b83abe5e17a40a6eec70e4b4ff67d0da1f
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2023-04-12 11:03:22 -0500

    Update ficha.py

--- logica/ficha.py
+++ logica/ficha.py
@@ -1,4 +1,5 @@
 class Ficha:
-    def __init__(self, numC1, numC2):
-        self.numC1 = numC1  #Numero de la cara 1
-        self.numC2 = numC2  #Numero de la cara 2
+    def __init__(self, Valor1, Valor2, Imagen=""):
+        self.Valor1 = Valor1  #Numero de la cara 1
+        self.Valor2  = Valor2   #Numero de la cara 2
+        self.Imagen = Imagen
