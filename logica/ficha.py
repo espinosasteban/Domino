2d841d81f03157bb46e23fa167b80bbd7283991f
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2023-04-12 17:02:56 -0500

    atributos

--- logica/ficha.py
+++ logica/ficha.py
@@ -1,5 +1,9 @@
 class Ficha:
-    def __init__(self, Valor1, Valor2, Imagen=""):
-        self.Valor1 = Valor1  #Numero de la cara 1
-        self.Valor2  = Valor2   #Numero de la cara 2
-        self.Imagen = Imagen
+    def __init__(self, valor1, valor2, imagen=""):
+        self.__valor1 = valor1  #Numero de la cara 1
+        self.__valor2  = valor2   #Numero de la cara 2
+        self.__imagen = imagen
+
+    def getValores(self):
+        vals = tuple(self.__valor1, self.__valor2)
+        return vals
--- logica/jugador.py
+++ logica/jugador.py
@@ -1,4 +1,4 @@
 class Jugador:
     def __init__(self, nombre, fichas): #Mirar si poner fichas en el constructor
-        self.nombre = nombre
-        self.fichas = []
+        self.__nombre = nombre
+        self.__fichas = fichas
